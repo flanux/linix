ab9186ed28db9802d901b9e78b0c109a6b139626
flanux <forlinux40@gmail.com>
2026-08-21 15:54:54 +0545

    added impl of touch command

---
+++ src/commands/touch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <linix/command.h>
+
+int linix_cmd_touch(int argc, char **argv)
+{
+	if (argc < 2) {
+		fpritnf(stderr, "linix touch: missing file opearand");
+		return 1;
+	}
+
+	int fd = open(
+		argv[1],
+		O_CREAT | O_WRONLY,
+		0644
+	);
+
+	if (fd == -1) {
+		perror("linix touch");
+		return 1;
+	}
+
+	close(fd);
+	return 0;
+}
